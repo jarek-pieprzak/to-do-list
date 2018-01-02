72e122b74a8e53ec027cead8b3bbb0d612259d72
Jarek Pieprzak <jarekpiep@gmail.com>
2018-01-02 22:16:37 +0100

    add new list item but without value (defaultValue: '')

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 class App extends Component {
 
     state = {
+        defaultValue: '',
         tasks: [
             {
                 id: 1,
@@ -19,11 +20,33 @@ class App extends Component {
         ]
     };
 
+    handleSubmit = (event) => {
+        event.preventDefault();
+        this.setState({
+            tasks: this.state.tasks.concat({
+                id: this.state.tasks.map(
+                    task => task.id
+                ).reduce(
+                    (biggest, next) => Math.max(biggest, next),
+                    0
+                ) + 1,
+                content: this.state.defaultValue
+            })
+        })
+    };
+
     render() {
         return (
             <div>
               <h1>Task List</h1>
-              <ul>
+
+                <form onSubmit={this.handleSubmit}>
+                    <input />
+                    <button>Add task</button>
+                </form>
+
+
+                <ul>
                   {
                       this.state.tasks.map(function (task) {
                               return (
